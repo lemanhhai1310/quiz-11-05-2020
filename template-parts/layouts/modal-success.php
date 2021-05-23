9193a49f3fa8bc10e65124718a698d7a0443bc16
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-05-23 16:55:45 +0700

    update

---
+++ template-parts/layouts/modal-success.php
@@ -0,0 +1,7 @@
+<!-- This is the modal with the outside close button -->
+<div id="modal-ketqua-outside" class="uk-text-center modal__ketqua" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body">
+        <button class="uk-modal-close-outside" type="button" uk-close></button>
+        <p class="modal__ketqua__desc">Bạn có tố chất và thái độ cần thiết để thành công với tư cách là một kỹ sư phần mềm. Bạn sở hữu óc tư duy phân tích thiên bẩm, không bị nhụt chí trước những vấn đề khó khăn và sẵn sàng đầu tư thời gian để cập nhật kỹ năng của mình. Bạn nên nghiêm túc xem xét phát triển sự nghiệp trong lĩnh vực này!</p>
+    </div>
+</div>
